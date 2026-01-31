77965f43825c47b3ebc8bb5199f25cc5f4c469ad
emioiso <jijjjiijiijijiji@gmail.com>
2026-01-31 19:59:13 +0900

    Initial Commit

---
+++ YUSOKU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YUSOKU
+//
+//  Created by emi oiso on 2026/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YUSOKUTests/YUSOKUTests.swift
@@ -0,0 +1,17 @@
+//
+//  YUSOKUTests.swift
+//  YUSOKUTests
+//
+//  Created by emi oiso on 2026/01/31.
+//
+
+import Testing
+@testable import YUSOKU
+
+struct YUSOKUTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

f1b8b956d539089c17a4edd85de18d7d1e6167f7
emioiso <jijjjiijiijijiji@gmail.com>
2026-02-08 17:40:01 +0900

    YUKIROKUkunRoomViewControllerにテスト広告がつきました。

---
+++ YUSOKU/YUKIROKUkunRoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  YUKIROKUkunRoomViewController.swift
+//  YUSOKU
+//
+//  Created by emi oiso on 2026/02/08.
+//
+
+import UIKit
+
+class YUKIROKUkunRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
